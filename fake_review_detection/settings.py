986978f968f1e26e994aa73c3c9c16b4c9663818
NASIR ABBAS <85347981+nasirabbas827@users.noreply.github.com>
2026-06-02 11:43:14 +0500

    security: remove exposed credentials

--- fake_review_detection/settings.py
+++ fake_review_detection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=3pp)b_yuc=moe2#dcz78-d^^ub_z%jn-i-1k=03cinj!zik+'
+SECRET_KEY = "YOUR_OWN_API_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
